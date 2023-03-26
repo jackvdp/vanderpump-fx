2dfd5beeb930d78f02cb699f9cdb8f858562955b
jackvdp <jack@pumpymusic.co.uk>
2023-03-26 13:46:27 +0100

    Add files and working navbar

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,87 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
+import appData from "../../data/app.json";
+import { handleDropdown, handleMobileDropdown } from "../../common/navbar";
+
+const Navbar = ({ lr, nr, theme }) => {
+  return (
+    <nav
+      ref={nr}
+      className={`navbar navbar-expand-lg change ${
+        theme === "themeL" ? "light" : ""
+      }`}
+    >
+      <div className="container">
+
+        <Link href="/">
+          <a>
+            {theme ? (
+              theme === "themeL" ? (
+                <img ref={lr} src={appData.darkLogo} alt="logo" width={100} height={26}/>
+              ) : (
+                <img ref={lr} src={appData.lightLogo} alt="logo" />
+              )
+            ) : (
+              <img ref={lr} src={appData.lightLogo} alt="logo" />
+            )}
+          </a>
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleMobileDropdown}
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="icon-bar">
+            <i className="fas fa-bars"></i>
+          </span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+
+            {/* FX Services */}
+
+            <li className="nav-item">
+              <Link href={`/about`}>
+                <a className="nav-link">FX Services</a>
+              </Link>
+            </li>
+
+            {/* Other Services */}
+
+            <li className="nav-item">
+              <Link href={`/about`}>
+                <a className="nav-link">Other Services</a>
+              </Link>
+            </li>
+
+            {/* Open an Account */}
+
+            <li className="nav-item">
+              <Link href={`/about`}>
+                <a className="nav-link">Open an Account</a>
+              </Link>
+            </li>
+
+            {/* Speak to an Expert */}
+
+            <li className="nav-item">
+              <Link href={`/contact`}>
+                <a className="nav-link">Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
